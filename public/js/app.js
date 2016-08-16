86a35c870d08e566f37f6f49af704f830fbbb796
dpcc <drewpcchannel@gmail.com>
2016-08-16 11:12:56 -1000

    nothing

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,28 @@
 var counterApp = angular.module('counterApp', ['ui.router']);
+
+var chart =
+  c3.generate({
+      data: {
+          columns: [
+              ["some college credit, no degree", 2268],
+              ["high school diploma or equivalent (GED)", 1356],
+              ["bachelor's degree", 5644],
+              ["master's degree (non-professional)", 1445],
+              ["professional degree (MBA, MD, JD, etc.)", 692],
+              ["trade, technical, or vocational training", 443],
+              ["associate's degree", 649],
+              ["some high school", 764],
+              ["Ph.D.", 160],
+              ["no high school (secondary school)", 258],
+          ],
+          type : 'pie',
+          onclick:
+            function () {chart.resize({height:700, width:700});},
+          onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+          onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+      }
+  });
+
 counterApp.config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
       $stateProvider
@@ -10,7 +34,7 @@ counterApp.config(['$stateProvider', '$urlRouterProvider',
           url:"/list",
           templateUrl: "partials/state1.list.html",
           controller: function($scope) {
-            $scope.items = ['a', 'b', 'c', 'd'];
+            $scope.items = ['a', 'b', 'c', 'd', 'e'];
           }
         })
         .state('state2', {
@@ -33,3 +57,4 @@ counterApp.controller('counterController', ['$scope', function($scope) {
         $scope.count++;
     };
 }]);
+
